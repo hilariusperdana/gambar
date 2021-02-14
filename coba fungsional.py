daf356d59663a31c2a741f27fadf782b77c68dcd
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-14 22:14:26 +0700

    commit

--- coba fungsional.py
+++ coba fungsional.py
@@ -5,3 +5,21 @@ print(polynominal(-4))
 
 #lambda
 print ((lambda x: x*2 + 5*x + 4) (-4))
+
+
+#iter
+# list huruf vokal
+vokal = ['a', 'i', 'u', 'e', 'o']
+
+vokal_iter = iter(vokal)
+# print 'a'
+print(next(vokal_iter))
+# print 'i'
+print(next(vokal_iter))
+# print 'u'
+print(next(vokal_iter))
+# print 'e'
+print(next(vokal_iter))
+# print 'o'
+print(next(vokal_iter))
+#https://www.pythonindo.com/fungsi-iter/#:~:text=Fungsi%20iter()%20berfungsi%20menghasilkan,di%20iterasikan%20satu%20persatu%20elemen.&text=Fungsi%20iter()%20memiliki%20dua,set%2C%20dan%20lain%2Dlain)
